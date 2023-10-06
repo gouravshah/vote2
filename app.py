3223d6a886cc618eacf024c94e08a77636aeff4c
Gourav Shah <gs@initcron.org>
2023-10-06 11:28:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Emacs")
-option_b = os.getenv('OPTION_B', "Vi")
+option_a = os.getenv('OPTION_A', "EKS")
+option_b = os.getenv('OPTION_B', "AKS")
 hostname = socket.gethostname()
-version = 'v1'
+version = 'v0.3.1'
 
 app = Flask(__name__)
 
